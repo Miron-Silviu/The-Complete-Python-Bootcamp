7b6ec01eb4eba11e94da274d1e831e963bb5a1bd
Silviu <silviu_miron_90@outlook.com>
2025-08-22 22:57:40 +0100

    Set up the project for day 1

---
+++ day1/challenges.py
@@ -0,0 +1,22 @@
+
+
+#INSTRUCTIONS
+
+
+#1
+# Write a program that prints the same notes
+# from the
+# previous lesson using what your have learned
+# about python print function
+
+
+print ("Day 1 -Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# 2  Fix the code below
+
+print("Day 1 -String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g print("Hello" "+" "World)')
+print("New lines can be created with a backslash and n.")
---
+++ day1/lesson.py
@@ -0,0 +1,12 @@
+
+# 1PRINT HELLO WORLD
+
+print("Hello World")
+
+
+# 2 STRING MANIPULATION
+print("Hello world!\nHello world\nHello world!")
+
+# 3 String Concatenation
+
+print("Hello" + " "+ "Silviu")
